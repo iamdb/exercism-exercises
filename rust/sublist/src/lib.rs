b48ce952a11f58912fe59b21b921008dfc8da4cd
David Benjamin <db@iamdb.co>
2022-09-26 14:18:49 -0700

    complete sublist

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -6,6 +6,28 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    if first_list.eq(second_list) {
+        Comparison::Equal
+    } else {
+        if first_list.is_empty() {
+            return Comparison::Sublist;
+        } else if second_list.is_empty() {
+            return Comparison::Superlist;
+        } else if first_list.len() < second_list.len() {
+            for window in second_list.windows(first_list.len()) {
+                if window.eq(first_list) {
+                    return Comparison::Sublist;
+                }
+            }
+        } else {
+            for window in first_list.windows(second_list.len()) {
+                if window.eq(second_list) {
+                    return Comparison::Superlist;
+                }
+            }
+        }
+
+        Comparison::Unequal
+    }
 }
